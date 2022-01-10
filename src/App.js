c1f166e47da5d95fbfe5bf22df2f12327dd42d32
Darshan K M <darshan.km13@gmail.com>
2022-01-10 21:07:25 +0530

    adding first component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+    // const [bool, setBool] = useState(false)
+    const [btnColor, setBtnColor] = useState('red')
+    const [btnDisabled, setBtnDisabled] = useState(false);
+    const newBtnColor = btnColor === 'red' ? 'blue' : 'red'
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button disabled={btnDisabled}   className={newBtnColor} onClick={()=>setBtnColor(newBtnColor)}>{`Change to ${btnColor}`}</button>
+        <input type="checkbox" onChange={(event)=> setBtnDisabled(event.target.checked)} aria-checked={btnDisabled} id={'buttonCheck'}/>
+            <label htmlFor="buttonCheck"> Toggle Button</label>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,57 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('button has initial color', () => {
+render(<App/>)
+    const button = screen.getByRole('button', {name:'Change to red'})
+    expect(button).toHaveClass('blue')
 });
+
+test('button turns blue when clicked', () => {
+    render(<App/>)
+    const button = screen.getByRole('button', {name:'Change to red'})
+    fireEvent.click(button)
+    expect(button).toHaveClass('red')
+    expect(button).toHaveTextContent('Change to blue')
+});
+
+test('by default button is enabled and checkbox is checked', () => {
+    render(<App/>)
+    const button = screen.getByRole('button', {name:'Change to red'})
+    const checkbox = screen.getByRole('checkbox')
+
+    expect(button).toBeEnabled()
+    expect(checkbox).not.toBeChecked()
+});
+
+test('checkbox disables on first click and enables on second click',
+    () => {
+        render(<App/>)
+        const button = screen.getByRole('button', {name: 'Change to red'})
+        const checkbox = screen.getByRole('checkbox', {name:'Toggle Button'})
+        fireEvent.click(checkbox)
+        expect(button).toHaveTextContent('Change to red')
+        expect(checkbox).toBeChecked()
+        expect(button).toBeDisabled()
+        fireEvent.click(checkbox)
+        expect(button).toBeEnabled()
+        expect(checkbox).not.toBeChecked()
+    });
+
+test('test css',
+    () => {
+        render(<App/>)
+        const button = screen.getByRole('button', {name: 'Change to red'})
+        const checkbox = screen.getByRole('checkbox', {name:'Toggle Button'})
+        fireEvent.click(checkbox)
+        expect(button).toHaveAttribute('disabled')
+        expect(checkbox).toBeChecked()
+        expect(button).toBeDisabled()
+        fireEvent.click(checkbox)
+        expect(button).toBeEnabled()
+        expect(checkbox).not.toBeChecked()
+        expect(button).toHaveClass('blue')
+        fireEvent.click(button)
+        expect(button).toHaveClass('red')
+        expect(button).toHaveTextContent('Change to blue')
+    });
