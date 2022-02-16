f49aa3cfd51a6ec432ef68127063736f594948aa
Darshan-km <darshan.km13@gmail.com>
2022-02-16 08:38:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
     <button disabled={btnDisabled}   className={newBtnColor} onClick={()=>setBtnColor(newBtnColor)}>{`Change to ${btnColor}`}</button>
         <input type="checkbox" onChange={(event)=> setBtnDisabled(event.target.checked)} aria-checked={btnDisabled} id={'buttonCheck'}/>
             <label htmlFor="buttonCheck"> Toggle Button</label>
+
     </div>
   );
 }
